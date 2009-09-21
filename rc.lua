a666717accdf91bcf59f709708dcaf2dfaf93d99
Konstantin Stepanov <kstep@p-nut.info>
2009-09-22 00:43:43 +0300

    use new lifty.widgets contructors in main config

--- rc.lua
+++ rc.lua
@@ -220,26 +220,26 @@ for s = 1, screen.count() do
 	bar_widget_params.width = 15
 	bar_widget_params.title = " <span color='%s'>%d%%</span> "
 	battery_widget[s] =
-		lifty.utils.new_bar_widget(bar_widget_params, {
+		lifty.widgets.common.progressbar(bar_widget_params, {
 			["bat0"] = { sensor = battery_sensor },
 		}).widgets
 
 	bar_widget_params.width = 20
 	bar_widget_params.title = " %0.1f/%0.1f "
 	thermal_widgets[s] =
-		lifty.utils.new_bar_widget(bar_widget_params, {
+		lifty.widgets.common.progressbar(bar_widget_params, {
 			["therm0"] = { sensor = thermal_sensors[1] },
 			["therm1"] = { sensor = thermal_sensors[2] },
 		}).widgets
 	bar_widget_params.title = " %s/%s "
 	cpufreq_widgets[s] =
-		lifty.utils.new_bar_widget(bar_widget_params, {
-			["cpufreq0"] = { sensor = cpufreq_sensors[1], period = 2, humanize = { base = 1000, postfixes = { "", "MHz", "GHz" } } },
-			["cpufreq1"] = { sensor = cpufreq_sensors[2], period = 2, humanize = { base = 1000, postfixes = { "", "MHz", "GHz" } } },
+		lifty.widgets.common.progressbar(bar_widget_params, {
+			["cpufreq0"] = { sensor = cpufreq_sensors[1], period = 2, humanize = { base = 1000, postfixes = { "Hz", "MHz", "GHz" } } },
+			["cpufreq1"] = { sensor = cpufreq_sensors[2], period = 2, humanize = { base = 1000, postfixes = { "Hz", "MHz", "GHz" } } },
 		}).widgets
 	bar_widget_params.title = " %d%%/%d%% "
 	cpuload_widgets[s] =
-		lifty.utils.new_bar_widget(bar_widget_params, {
+		lifty.widgets.common.progressbar(bar_widget_params, {
 			["cpuload0"] = { sensor = cpuload_sensors[1], period = 2 },
 			["cpuload1"] = { sensor = cpuload_sensors[2], period = 2 },
 		}).widgets

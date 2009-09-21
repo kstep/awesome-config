d04c62f3bd772adf97fdc4588f5e16a3fcf69f32
Konstantin Stepanov <kstep@p-nut.info>
2009-09-21 04:19:40 +0300

    added battery state colors into theme

--- themes/default/theme.lua
+++ themes/default/theme.lua
@@ -4,5 +4,9 @@ theme = dofile("/usr/local/share/awesome/themes/default/theme.lua")
 
 theme.font          = "terminus 8"
 
+theme.Full        = "#C4C871"
+theme.Charging    = "#39C83C"
+theme.Discharging = "#C84D4D"
+
 return theme
 -- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=80

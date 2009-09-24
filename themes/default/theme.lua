b7883bf43249e7c2b925279ae39ebca1b0239f5c
Konstantin Stepanov <kstep@p-nut.info>
2009-09-24 13:14:20 +0300

    overriden wallpaper

--- themes/default/theme.lua
+++ themes/default/theme.lua
@@ -1,3 +1,4 @@
+require("awful.util")
 -- Inherit default theme
 
 theme = dofile("/usr/local/share/awesome/themes/default/theme.lua")
@@ -7,6 +8,8 @@ theme.font          = "terminus 8"
 theme.Full        = "#C4C871"
 theme.Charging    = "#39C83C"
 theme.Discharging = "#C84D4D"
+
+theme.wallpaper_cmd = { "awsetbg " .. awful.util.getdir("config") .. "/themes/default/Desktopography-1800x1125.jpg" }
 
 return theme
 -- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=80

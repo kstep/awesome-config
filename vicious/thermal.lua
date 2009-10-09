d163991d782363d679150272cf03a32691f19cbe
Konstantin Stepanov <kstep@p-nut.info>
2009-10-10 01:13:54 +0300

    vicious.thermal: read data from /sys, not /proc
    Also uses new helpers.readfile and returns
    critical temperature (1st trip point) as second value.

--- vicious/thermal.lua
+++ vicious/thermal.lua
@@ -6,23 +6,29 @@
 -- {{{ Grab environment
 local io = { open = io.open }
 local setmetatable = setmetatable
+local helpers = require("vicious.helpers")
 -- }}}
 
 
 -- Thermal: provides temperature levels of ACPI thermal zones
 module("vicious.thermal")
 
+local max_temp = {}
+local basedir = "/sys/class/thermal/thermal_zone"
 
 -- {{{ Thermal widget type
 local function worker(format, thermal_zone)
     -- Get thermal zone
-    local f = io.open("/proc/acpi/thermal_zone/" .. thermal_zone .. "/temperature")
-    local line = f:read("*line")
-    f:close()
+    local line = helpers.readfile(basedir .. thermal_zone .. "/temp", "*n")
 
-    local temperature = line:match("[%d]?[%d]?[%d]")
+    if max_temp[thermal_zone] == nil then
+        local line = helpers.readfile(basedir .. thermal_zone .. "/trip_point_0_temp", "*n") or 100000
+        max_temp[thermal_zone] = line / 1000
+    end
 
-    return {temperature}
+    local temperature = line / 1000
+
+    return {temperature, max_temp[thermal_zone]}
 end
 -- }}}
 

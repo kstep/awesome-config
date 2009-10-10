16c6e3651b474e54f8dfd16f91b745040c9cedba
Konstantin Stepanov <kstep@p-nut.info>
2009-10-10 03:19:56 +0300

    fixed vicious.thermal

--- vicious/thermal.lua
+++ vicious/thermal.lua
@@ -20,13 +20,12 @@ local basedir = "/sys/class/thermal/thermal_zone"
 local function worker(format, thermal_zone)
     -- Get thermal zone
     local line = helpers.readfile(basedir .. thermal_zone .. "/temp", "*n")
+    local temperature = line / 1000
 
     if max_temp[thermal_zone] == nil then
         local line = helpers.readfile(basedir .. thermal_zone .. "/trip_point_0_temp", "*n") or 100000
         max_temp[thermal_zone] = line / 1000
     end
-
-    local temperature = line / 1000
 
     return {temperature, max_temp[thermal_zone]}
 end

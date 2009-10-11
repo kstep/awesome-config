6aa46e6c57ec851198fae4a644b0f454548fa6ac
Konstantin Stepanov <kstep@p-nut.info>
2009-10-11 20:55:59 +0300

    vicious.formatters.* always get table as sensors result

--- vicious/formatters.lua
+++ vicious/formatters.lua
@@ -15,19 +15,19 @@ function format(format, args)
     return format
 end
 
-function scale(widget, arg, meta)
-    local value = arg
+function scale(widget, args, meta)
+    local value = args[1]
     local max = meta.max or 100
     local min = meta.min or 0
 
     return (value - min) / (max - min)
 end
 
-function humanize(widget, arg, meta)
+function humanize(widget, args, meta)
     local suffixes = meta.suffixes or { "b", "Kb", "Mb", "Gb", "Tb" }
     local scale = meta.scale or 1024
     local init = meta.init or 1
-    local value = arg
+    local value = args[1]
 
     local suffix = init
     while value > scale and suffix < #suffixes do

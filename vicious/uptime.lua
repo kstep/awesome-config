2aea752f31764e820eb7455e9dc14c7269a82938
Konstantin Stepanov <kstep@p-nut.info>
2009-10-12 11:15:20 +0300

    made vicious.uptime() sensor conform new interface requirements
    Also use vicious.formatters.hms() to get days/hours/min/secs out from raw seconds
    returned by the sensor.

--- vicious/uptime.lua
+++ vicious/uptime.lua
@@ -9,27 +9,22 @@ local tonumber = tonumber
 local io = { open = io.open }
 local setmetatable = setmetatable
 local math = { floor = math.floor }
+local helpers = require("vicious.helpers")
 -- }}}
 
 
 -- Uptime: provides system uptime information
 module("vicious.uptime")
 
+function meta() return {} end
 
 -- {{{ Uptime widget type
 local function worker(format)
     -- Get /proc/uptime
-    local f = io.open("/proc/uptime")
-    local line = f:read("*line")
-    f:close()
-
+    local line = helpers.readfile("/proc/uptime", "*line")
     local total_uptime   = math.floor(tonumber(line:match("[%d%.]+")))
-    local uptime_days    = math.floor(total_uptime   / (3600 * 24))
-    local uptime_hours   = math.floor((total_uptime  % (3600 * 24)) / 3600)
-    local uptime_minutes = math.floor(((total_uptime % (3600 * 24)) % 3600) / 60)
-    local uptime_seconds = math.floor(((total_uptime % (3600 * 24)) % 3600) % 60)
-
-    return {total_uptime, uptime_days, uptime_hours, uptime_minutes, uptime_seconds}
+    -- use vicious.formatters.hms() to get days/hours/mins/secs
+    return { total_uptime }
 end
 -- }}}
 

62cee099be6d37d0728fe50ff1b04be7314bc681
Konstantin Stepanov <kstep@p-nut.info>
2009-11-30 15:31:59 +0200

    sensor.mixer: fixed mixer.lua module

--- sensual/mixer.lua
+++ sensual/mixer.lua
@@ -1,14 +1,13 @@
 local setmetatable = setmetatable
 local ipairs = ipairs
 local table = { insert = table.insert }
+local mixer = assert(package.loadlib("/home/kstep/.config/awesome/sensual/mixer.so", "luaopen_mixer"))()
 
 module("sensual.mixer")
 
-assert(package.loadlib("mixer.so", "luaopen_mixer"))()
-
 function worker(self)
    local volumes = {}
-   for _, dev in ipairs(sensor.devices) do
+   for _, dev in ipairs(self.devices) do
        table.insert(volumes, dev[1])
        table.insert(volumes, dev.muted)
    end
@@ -19,7 +18,7 @@ local sensor
 local function new(mixno, ...)
     if not sensor then
         sensor = {}
-        sensor.meta = {}
+        sensor.meta = { min = 0, max = 100 }
         sensor.args = mixno
         sensor.mixer = mixer.open(mixno)
         sensor.devices = {}
@@ -31,4 +30,4 @@ local function new(mixno, ...)
     return sensor
 end
 
-setmetatable(_M, { __call = function(_, ...) new(...) end })
+setmetatable(_M, { __call = function(_, ...) return new(...) end })

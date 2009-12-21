518dc51b429448e539a99dd667bb0833db27516a
Konstantin Stepanov <kstep@p-nut.info>
2009-12-21 13:46:57 +0200

    sensual.wifi: refactored wifi sensor

--- sensual/wifi.lua
+++ sensual/wifi.lua
@@ -4,7 +4,7 @@
 ----------------------------------------------------------
 
 -- {{{ Grab environment
-local io = { popen = io.popen }
+local io = { open = io.open }
 local setmetatable = setmetatable
 local string = {
     find = string.find,
@@ -16,49 +16,38 @@ local string = {
 -- Wifi: provides wireless information for a requested interface
 module("sensual.wifi")
 
-
--- {{{ Wireless widget type
-local function worker(format, iface)
+local function worker(self)
     -- Get data from iwconfig (where available)
-    local f = io.popen("iwconfig " .. iface)
-    local iw = f:read("*all")
-    f:close()
+    local f = io.open("/proc/net/wireless")
+    local data = {}
 
-    -- Default values
-    local winfo = {
-        ["{ssid}"] = "N/A",
-        ["{mode}"] = "N/A",
-        ["{chan}"] = "N/A",
-        ["{rate}"] = "N/A",
-        ["{link}"] = "N/A",
-        ["{sign}"] = "N/A"
-    }
-
-    -- Check if iwconfig wasn't found, can't be executed or the
-    -- interface is not a wireless one
-    if iw == nil or string.find(iw, "No such device") then
-        return winfo
-    else
-        -- The output differs from system to system, some stats can
-        -- be separated by =, and not all drivers report all stats
-        winfo["{ssid}"] =  -- SSID can have almost anything in it
-          string.match(iw, 'ESSID[=:]"([%w%p]+[%s]*[%w%p]*]*)"') or winfo["{ssid}"]
-        winfo["{mode}"] =  -- Modes are simple, but also match the "-" in Ad-Hoc
-          string.match(iw, "Mode[=:]([%w%-]*)") or winfo["{mode}"]
-        winfo["{chan}"] =  -- Channels are plain digits
-          string.match(iw, "Channel[=:]([%d]+)") or winfo["{chan}"]
-        winfo["{rate}"] =  -- Bitrate can start with a space and we want to display Mb/s
-          string.match(iw, "Bit Rate[=:]([%s]?[%d%.]*[%s][%/%a]+)") or winfo["{rate}"]
---      winfo["{link}"] =  -- Link quality can contain a slash: 32/100
---        string.match(iw, "Link Quality[=:]([%d]+[%/%d]*)") or winfo["{link}"]
-        winfo["{link}"] =  --  * match only the first number, great data for a progressbar
-          string.match(iw, "Link Quality[=:]([%d]+)") or winfo["{link}"]
-        winfo["{sign}"] =  -- Signal level can be a negative value, also display decibel notation
-          string.match(iw, "Signal level[=:]([%-%d]+[%s][%a]*)") or winfo["{sign}"]
+    if f then
+        for line in f:lines() do
+            if line:find(self.iface) then
+                local iface, state, link, level, noise, nwid, crypt, frag, retry, misc, beacon =
+                    line:match("^%s+([a-z0-9]+):%s+([0-9]+)%s+([0-9.]+)%s+(-[0-9]+)%s+(-[0-9]+)%s+([0-9]+)%s+([0-9]+)%s+([0-9]+)%s+([0-9]+)%s+([0-9]+)%s+([0-9]+)")
+                data["state"] = state
+                data["link"]  = link
+                data["level"] = level
+                data["noise"] = noise
+                break
+            end
+        end
+        f:close()
     end
 
-    return winfo
+    return data
 end
--- }}}
 
-setmetatable(_M, { __call = function(_, ...) return worker(...) end })
+local sensors = {}
+function new(iface)
+    if not sensors[iface] then
+        sensor = { iface = iface, meta = {} }
+        setmetatable(sensor, { __call = worker })
+        sensors[iface] = sensor
+    end
+    return sensors[iface]
+end
+
+setmetatable(_M, { __call = function(_, ...) return new(...) end })
+

7c32b8dca0460c200dc36edf453be39dee4c3748
Konstantin Stepanov <kstep@p-nut.info>
2010-01-01 18:59:23 +0200

    sensual.dio: reworked dio sensor according to new interfaces

--- sensual/dio.lua
+++ sensual/dio.lua
@@ -10,65 +10,39 @@ local io = { open = io.open }
 local setmetatable = setmetatable
 local math = { floor = math.floor }
 local table = { insert = table.insert }
+local helpers = require("sensual.helpers")
 -- }}}
-
 
 -- Disk I/O: provides I/O statistics for requested storage devices
 module("sensual.dio")
 
-
--- Initialise function tables
-local disk_usage = {}
-local disk_total = {}
-
--- {{{ Disk I/O widget type
-local function worker(format, disk)
-    -- Get /proc/diskstats
-    local f = io.open("/proc/diskstats")
-    local disk_lines = {}
-
-    for line in f:lines() do
-        if line:match("("..disk..")%s") then
-            -- Todo: find a way to do this
-            --for stat in line:gmatch("%s([%d]+)") do
-            --    table.insert(disk_lines, stat)
-            --end
-            --
-            -- Skip first two matches
-            local stat = line:gmatch("%s([%d]+)")
-            stat()
-            stat()
-            -- Store the rest
-            for i = 1, 11 do
-                table.insert(disk_lines, stat())
-            end
-        end
-    end
-    f:close()
-
-    -- Ensure tables are initialized correctly
-    while #disk_total < #disk_lines do
-        table.insert(disk_total, 0)
-    end
-
-    local diff_total  = {}
-
-    for i, v in ipairs(disk_lines) do
-        -- Diskstats are absolute, substract our last reading
-        diff_total[i] = v - disk_total[i]
-
-        -- Store totals
-        disk_total[i] = v
-    end
-
-    -- Calculate I/O
-    disk_usage["{raw}"] = diff_total[7] + diff_total[3]
-    -- Divide "sectors read" by 2 and 1024 to get KB and MB
-    disk_usage["{kb}"] = math.floor(diff_total[7] + diff_total[3])/2
-    disk_usage["{mb}"] = math.floor(diff_total[7] + diff_total[3])/1024
-
-    return disk_usage
+function worker(self)
+    local line = helpers.readfile("/sys/class/block/" .. self.args .. "/stat", "*l")
+    local data = { line:match("(%d+)%s+(%d+)%s+(%d+)%s+(%d+)%s+(%d+)%s+(%d+)%s+(%d+)%s+(%d+)%s+(%d+)%s+(%d+)%s+(%d+)") }
+    local stat = {}
+    stat.reads       = data[1]
+    stat.tot_reads   = data[2]
+    stat.sec_read    = data[3]
+    stat.bytes_read  = data[3]*512
+    stat.ms_read     = data[4]
+    stat.writes      = data[5]
+    stat.tot_writes  = data[6]
+    stat.sec_write   = data[7]
+    stat.bytes_write = data[7]*512
+    stat.ms_write    = data[8]
+    stat.ios         = data[9]
+    stat.ms_ios      = data[10]
+    stat.tot_ms_ios  = data[11]
+    return stat
 end
--- }}}
 
-setmetatable(_M, { __call = function(_, ...) return worker(...) end })
+local sensors = {}
+local function new(dev)
+    if not sensors[dev] then
+        sensors[dev] = { args = dev, meta = {} }
+        setmetatable(sensors[dev], { __call = worker })
+    end
+    return sensors[dev]
+end
+
+setmetatable(_M, { __call = function(_, ...) return new(...) end })

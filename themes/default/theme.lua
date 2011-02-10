4fd0c8b9b7e459c30acb12f0cc92f33537daef1e
Konstantin Stepanov <kstep@p-nut.info>
2011-02-10 09:51:08 +0200

    lighter theme

--- themes/default/theme.lua
+++ themes/default/theme.lua
@@ -3,6 +3,22 @@ require("awful.util")
 
 theme = dofile("/usr/share/awesome/themes/zenburn/theme.lua")
 
+-- Zhurazlik theme from https://awesome.naquadah.org/wiki/Zhuravlik_theme
+theme.bg_normal     = "#f7f7f7"
+theme.bg_focus      = "#535d6c"
+theme.bg_urgent     = "#ff0000"
+theme.bg_minimize   = "#444444"
+
+theme.fg_normal     = "#535d6c"
+theme.fg_focus      = "#ffffff"
+theme.fg_urgent     = "#ffffff"
+theme.fg_minimize   = "#ffffff"
+
+theme.border_width  = "1"
+theme.border_normal = "#000000"
+theme.border_focus  = "#535d6c"
+
+-- My own config
 theme.font          = "terminus 8"
 
 -- Palette generated with http://www.colorschemer.com/online.html,
@@ -11,21 +27,37 @@ theme.font          = "terminus 8"
 -- 5, 6, 7, 8,
 -- 9, 10, 11,
 -- 14, 15, 16.
+--theme.palette = {
+    --"#EFDEAE", --  1 peach
+    --"#E0EFAE", --  2 marshal
+    --"#BFEFAE", --  3 sea green
+    --"#AEEFBD", --  4 blue green
+    --"#EFBDAE", --  5 pastel pink
+    --"#E6C97A", --  6 light brown
+    --"#DCB447", --  7 dark brown
+    --"#AEEFDE", --  8 cyan
+    --"#EFAEBF", --  9 purple
+    --"#476FDC", -- 10 blue
+    --"#7A97E6", -- 11 light blue
+    --"#DEAEEF", -- 12 light violet
+    --"#BDAEEF", -- 13 violet
+    --"#AEBFEF", -- 14 gray blue
+--}
 theme.palette = {
-    "#EFDEAE", --  1 peach
-    "#E0EFAE", --  2 marshal
-    "#BFEFAE", --  3 sea green
-    "#AEEFBD", --  4 blue green
-    "#EFBDAE", --  5 pastel pink
-    "#E6C97A", --  6 light brown
-    "#DCB447", --  7 dark brown
-    "#AEEFDE", --  8 cyan
-    "#EFAEBF", --  9 purple
-    "#476FDC", -- 10 blue
-    "#7A97E6", -- 11 light blue
-    "#DEAEEF", -- 12 light violet
-    "#BDAEEF", -- 13 violet
-    "#AEBFEF", -- 14 gray blue
+    "#A15050", -- 1
+    "#408040", -- 2
+    "#306060", -- 3
+    "#A17550", -- 4
+    "#814065", -- 5
+    "#949494", -- 6
+    "#A05050", -- 7
+    "#7C954B", -- 8
+    "#306060", -- 9
+    "#804064", -- 10
+    "#C078A0", -- 11
+    "#A07450", -- 12
+    "#59386B", -- 13
+    "#7C954A", -- 14
 }
 
 theme.kbd = {

aa3900e9bbadf9ffda1e0752729dbf71611eda7d
Konstantin Stepanov <kstep@p-nut.info>
2011-03-14 00:19:56 +0200

    default browser changed from firefox to luakit

--- rc/vars.lua
+++ rc/vars.lua
@@ -32,7 +32,8 @@ terminal_cmd = terminal .. " -e tmux"
 editor       = getenv("EDITOR") or "vim"
 editor_cmd   = terminal .. " -e " .. editor
 
-browser      = "/usr/bin/firefox"
+--browser      = "/usr/bin/firefox"
+browser      = "/usr/bin/luakit"
 browser_cmd  = browser
 
 player       = "/usr/bin/smplayer"

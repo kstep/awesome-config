47ad75f8f750b883235f7aff39cbe8ef309be068
Konstantin Stepanov <kstep@p-nut.info>
2011-04-28 01:36:33 +0300

    keyboard widget: mouse click handling + full background

--- rc/widget/keyboard.lua
+++ rc/widget/keyboard.lua
@@ -2,19 +2,23 @@
 local capi = { widget = widget, dbus = dbus }
 local theme = require("beautiful")
 local dbus = require("simpledbus")
+local button = require("awful.button")
 local setmetatable = setmetatable
 
 module("rc.widget.keyboard")
 
 widget = capi.widget({ type = "textbox" })
-layouts = { [0] = "<span color='#FFFFFF' bgcolor='" .. theme.kbd[0] .. "'> En </span>", [1] = "<span color='#FFFFFF' bgcolor='" .. theme.kbd[1] .. "'> Ru </span>" }
-widget.text = layouts[0]
+layouts = { [0] = " En ", [1] = " Ru " }
+local function show_layout(layout)
+    widget.text = '<span color="#FFFFFF">' .. (layouts[layout] or " ?? ") .. '</span>'
+    widget.bg = theme.kbd[layout] or "#000000"
+end
+show_layout(0)
 
 capi.dbus.request_name("session", "ru.gentoo.kbdd")
 capi.dbus.add_match("session", "interface='ru.gentoo.kbdd',member='layoutChanged'")
 capi.dbus.add_signal("ru.gentoo.kbdd", function (src, layout)
-    widget.text = layouts[layout]
-    widget.background_color = theme.kbd[layout]
+    show_layout(layout)
 end)
 
 local bus = dbus.SessionBus()
@@ -24,6 +28,8 @@ end
 function prev_layout()
     return bus:call_method('ru.gentoo.KbddService', '/ru/gentoo/KbddService', 'ru.gentoo.kbdd', 'prev_layout', true)
 end
+
+widget:buttons(button({ }, 1, prev_layout))
 
 setmetatable(_M, { __call = function (_, ...) return widget end })
 

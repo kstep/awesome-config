0a7d57aed0a9ed9a089f9d26be39d2facfb5f097
Konstantin Stepanov <me@kstep.me>
2011-06-14 11:30:18 +0300

    sensual: load sensor update

--- sensual/load.lua
+++ sensual/load.lua
@@ -14,17 +14,28 @@ module("sensual.load")
 
 
 -- {{{ Load widget type
-local function worker(format)
+local function worker(widget)
     -- Get load averages
     local f = io.open('/proc/loadavg')
     local line = f:read("*line")
     f:close()
 
-    local avg1, avg5, avg15 = 
-        line:match("([%d]*%.[%d]*)%s([%d]*%.[%d]*)%s([%d]*%.[%d]*)")
+    local avg1, avg5, avg15, running_procs, total_procs, recent_pid = 
+        line:match("([%d]*%.[%d]*)%s([%d]*%.[%d]*)%s([%d]*%.[%d]*)%s([%d]*)/([%d]*)%s([%d]*)")
 
-    return {avg1, avg5, avg15}
+    return {avg1, avg5, avg15, running_procs, total_procs, recent_pid}
 end
 -- }}}
 
-setmetatable(_M, { __call = function(_, ...) return worker(...) end })
+local sensor
+local function new()
+    if not sensor then
+        sensor = {}
+        sensor.meta = {min = 0, max = 3}
+        setmetatable(sensor, { __call = worker })
+    end
+    return sensor
+end
+
+
+setmetatable(_M, { __call = function(_, ...) return new(...) end })
